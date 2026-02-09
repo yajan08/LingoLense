33936457795c8223e425e9e63f5580cf0e2211fa
Yajan <yajanmehta@gmail.com>
2026-02-09 16:15:12 +0530

    Initial commit

---
+++ LingoLense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LingoLense
+//
+//  Created by SDC-USER on 09/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LingoLense/LingoLenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoLenseApp.swift
+//  LingoLense
+//
+//  Created by SDC-USER on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LingoLenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
