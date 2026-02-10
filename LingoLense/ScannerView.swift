49077472e52e4fc923a11024ff9a278892ea8137
Yajan <yajanmehta@gmail.com>
2026-02-10 16:21:20 +0530

    day 1

---
+++ LingoLense/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  LingoLense
+//
+//  Created by SDC-USER on 10/02/26.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
